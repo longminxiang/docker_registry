653e62c88b04528bdb6c75ca17eb99b2d530fdbb
eric <longminxiang@163.com>
2016-06-14 23:27:49 +0800

    big update

--- django/conf/conf.py
+++ django/conf/conf.py
@@ -1,42 +1,61 @@
 # -*- coding: utf-8 -*-
 import os
-import re
 
-DEFAULT_APP_NAME = 'mysite'
+DEFAULT_APP_NAME = 'website'
 
-APP_DIR = '/home/app/'
-CONF_DIR = '/opt/'
+APP_ROOT = '/home/'
+
+UWSGI_CONF_PATH = '/opt/conf/uwsgi.ini'
+
+
+def get_first_dir(p):
+    try:
+        p = p + "/" if p[-1] != "/" else p
+        a = os.listdir(p)
+        for x in a:
+            if os.path.isdir(p + x):
+                return x
+    except Exception, e:
+        print e
 
 
 class DjangoApp(object):
 
     def __init__(self):
         super(DjangoApp, self).__init__()
-        self.app_name = DEFAULT_APP_NAME
+        self.app_name = get_first_dir(APP_ROOT)
+        if self.app_name is None:
+            self.app_name = DEFAULT_APP_NAME
+        self.app_dir = APP_ROOT + self.app_name + "/"
 
+    # 如果项目不存在，创建一个新项目
     def new_app_if_needed(self):
-        fn = '%s%s' % (APP_DIR, 'manage.py')
+        fn = '%s%s' % (self.app_dir, 'manage.py')
         if not os.path.exists(fn):
+            os.mkdir(self.app_dir)
             os.system("echo \"create new app %s\"" % self.app_name)
-            os.system("django-admin.py startproject %s %s" % (self.app_name, APP_DIR))
-        else:
-            output = []
-            f = open(r"%s" % fn)
-            for i in f.readlines():
-                output.append(i)
-                f.close()
-            ostr = "".join(output)
-            p = re.compile(r'(, ")(.*)(\.settings)')
-            match = p.search(ostr)
-            self.app_name = match.groups()[1]
+            os.system("django-admin.py startproject %s %s" % (self.app_name, self.app_dir))
 
-    def app_path(self):
-        return '%s%s' % (APP_DIR, self.app_name)
+    # 创建uwsgi配置
+    def create_uwsgi_config(self):
+        if os.path.exists(UWSGI_CONF_PATH):
+            return
+        f = open(UWSGI_CONF_PATH, 'w')
+        nstr = "[uwsgi]\n \
+                http = :8888\n \
+                chdir = %s\n \
+                wsgi-file = %s/wsgi.py\n \
+                processes = 2\n \
+                threads = 1\n" % (self.app_dir, self.app_name)
+        f.write(nstr)
+        f.close()
 
     def start(self):
         self.new_app_if_needed()
-        os.system("echo \"start django\"")
-        os.system("python %s/manage.py runserver 0.0.0.0:8000" % APP_DIR)
+        os.system("echo yes | python %smanage.py collectstatic" % self.app_dir)
+        self.create_uwsgi_config()
+        os.system("echo \"run uwsgi\"")
+        os.system("uwsgi %s" % UWSGI_CONF_PATH)
 
 if __name__ == "__main__":
     app = DjangoApp()
