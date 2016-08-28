418046cad8c591562d1afb6851da9ab2f38685dc
eric <longminxiang@163.com>
2016-08-28 15:32:40 +0800

    update

--- mysql/opt/setup.sh
+++ mysql/opt/setup.sh
@@ -2,9 +2,15 @@
 
 if [ $MYSQL_OPEN_BACKUP -ne 1 ]; then
     echo "backup not open"
-    mkdir /backup/11$MYSQL_OPEN_BACKUP
     exit 0
 fi
+
+rm /tmp/cron
+
+#每3个小时备份一次
+echo "0 */3 * * * sh /opt/backup.sh" >> /tmp/cron
+
+crontab /tmp/cron
 
 service cron start
 
